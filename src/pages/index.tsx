3bb5e4a110032b41b435892a0b9d677c53c5ddbb
hayao <hayao@fascode.net>
2023-06-25 00:38:07 +0900

    Remove くそでか h1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,14 @@ import CommonLayout from "~/components/CommonLayout"
 export default function IndexPage() {
   return (
     <CommonLayout>
-      <div className="my-16 rounded-[2rem] border bg-lime-500 py-32 text-white">
+      {/*<div className="my-16 rounded-[2rem] border bg-lime-500 py-32 text-white">
         <h1 className="text-center font-inter text-8xl font-black">minskey</h1>
-      </div>
+      </div>*/}
+      <TimeLine />
     </CommonLayout>
   )
+}
+
+function TimeLine() {
+  return <></>
 }
