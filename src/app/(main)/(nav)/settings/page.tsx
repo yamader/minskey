7d6794180ec33fec781d69f99eedabc4da6a4630
山田ハヤオ <shun819.mail@gmail.com>
2024-01-07 04:08:54 +0900

    Add app setting (not work)

--- src/app/(main)/(nav)/settings/page.tsx
+++ src/app/(main)/(nav)/settings/page.tsx
@@ -1,31 +1,46 @@
 "use client"
 
 import { useAuth } from "~/features/auth"
+import Switch from "~/features/common/Switch"
 import { useSettings } from "~/features/settings"
 
 export default function SetingsPage() {
   const { account } = useAuth()
-  const [settings] = useSettings()
+  const [settings, setSetings] = useSettings()
 
   const h2class = "pt-6 pb-2.5 text-xl font-bold"
 
   return (
     <>
       <h1 className="pb-8 pt-16 text-4xl font-black">設定</h1>
-      <p>ほげほげ</p>
+
+      <section>
+        <h2 className={h2class}>アプリ設定</h2>
+        <Switch
+          init={settings.dark}
+          onChange={d => {
+            // なぜか設定が書き込まれない
+            setSetings({ ...settings, dark: d })
+          }}>
+          DarkMode
+        </Switch>
+      </section>
 
       {account && (
         <section>
           <h2 className={h2class}>トークンの設定</h2>
+          <p>現在準備中</p>
         </section>
       )}
 
       <hr />
 
-      <h2>生データ</h2>
-      <pre>
-        <code>{JSON.stringify(settings)}</code>
-      </pre>
+      <section>
+        <h2>生データ</h2>
+        <pre>
+          <code>{JSON.stringify(settings)}</code>
+        </pre>
+      </section>
     </>
   )
 }
