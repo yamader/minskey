77c0228911209dc59028d56e0efe87389d92f2b0
山田ハヤオ <shun819.mail@gmail.com>
2024-01-07 23:25:08 +0900

    Add playground page

---
+++ src/app/(main)/(nav)/playground/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useAccounts, useAuth } from "~/features/auth"
+
+export default function PlaygroundPage() {
+  const { accounts } = useAccounts()
+  const { setAuth } = useAuth()
+  const router = useRouter()
+
+  return (
+    <>
+      <h1 className="pb-8 pt-16 text-4xl font-black">Playground</h1>
+      <p>
+        <a href="/login/" className="underline ">
+          アカウントを追加
+        </a>
+      </p>
+      {accounts?.map(account => (
+        <div key={account.token}>
+          <p
+            onClick={() => {
+              setAuth({ account: account })
+              router.push("/home")
+            }}>
+            {account.host}
+          </p>
+        </div>
+      ))}
+    </>
+  )
+}
