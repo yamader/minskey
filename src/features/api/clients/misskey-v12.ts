f17405a44f74743a1a1ff8f15689608eee878549
山田ハヤオ <shun819.mail@gmail.com>
2024-03-01 00:52:14 +0900

    Fix in misskey-v13

--- src/features/api/clients/misskey-v12.ts
+++ src/features/api/clients/misskey-v12.ts
@@ -3,10 +3,10 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV12Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.post("meta")
-    if (!emojis || typeof emojis.find !== "function") return null
+    if (!emojis) return null
     return (
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      emojis.find((e: any) => {
+      emojis.find?.((e: any) => {
         if (!e) return false
         e.name == name
       })?.url ?? null
--- src/features/api/clients/misskey-v13.ts
+++ src/features/api/clients/misskey-v13.ts
@@ -3,6 +3,13 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV13Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.get("emojis")
-    return emojis.find((e: any) => e.name == name)?.url ?? null
+    if (!emojis) return null
+    return (
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      emojis.find((e: any) => {
+        if (!e) return false
+        return e.name == name
+      })?.url ?? null
+    )
   }
 }
