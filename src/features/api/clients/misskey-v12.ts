74a2e9fc8fde5a70e4eaa8ab8995cc86a1158a4a
山田ハヤオ <shun819.mail@gmail.com>
2024-02-29 23:40:53 +0900

    Fix checking method

--- src/features/api/clients/misskey-v12.ts
+++ src/features/api/clients/misskey-v12.ts
@@ -3,11 +3,12 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV12Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.post("meta")
-    if (!emojis || !Object.keys(emojis).includes("find")) return null
+    if (!emojis || typeof emojis.find !== "function") return null
     return (
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       emojis.find((e: any) => {
-        Object.keys(e).includes("name") && e.name == name
+        if (!e) return false
+        e.name == name
       })?.url ?? null
     )
   }
