6696876a401f19b3ddac9804e0eec5727308ea28
山田ハヤオ <shun819.mail@gmail.com>
2024-02-29 22:35:48 +0900

    Fix cannot read property of undefined

--- src/features/api/clients/misskey-v12.ts
+++ src/features/api/clients/misskey-v12.ts
@@ -3,6 +3,12 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV12Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.post("meta")
-    return emojis.find((e: any) => e.name == name)?.url ?? null
+    if (!emojis || !Object.keys(emojis).includes("find")) return null
+    return (
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      emojis.find((e: any) => {
+        Object.keys(e).includes("name") && e.name == name
+      })?.url ?? null
+    )
   }
 }
