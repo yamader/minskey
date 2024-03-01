450b7b645746c45395b591eda3793070c082e57e
YamaD <me@dyama.net>
2024-03-01 22:56:42 +0900

    closes #47; nullnull

--- src/features/api/clients/misskey-v12.ts
+++ src/features/api/clients/misskey-v12.ts
@@ -3,6 +3,6 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV12Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.post("meta")
-    return emojis.find((e: any) => e.name == name)?.url ?? null
+    return emojis?.find?.((e: any) => e.name == name)?.url ?? null
   }
 }
--- src/features/api/clients/misskey-v13.ts
+++ src/features/api/clients/misskey-v13.ts
@@ -3,6 +3,6 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV13Client extends MisskeyLatestClient {
   override async fetchEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.get("emojis")
-    return emojis.find((e: any) => e.name == name)?.url ?? null
+    return emojis?.find?.((e: any) => e.name == name)?.url ?? null
   }
 }
