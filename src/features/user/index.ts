97f797475a4ae326f961b30937443f09c34dcc51
山田ハヤオ <shun819.mail@gmail.com>
2024-03-05 18:28:54 +0900

    Use id as key

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -2,14 +2,16 @@ import { useAtom } from "jotai"
 import { atomWithStorage } from "jotai/utils"
 import { User } from "~/features/api/clients/entities"
 
-const logginedCacheAtom = atomWithStorage<User[]>("minsk::auth::loggined", [])
+const logginedCacheAtom = atomWithStorage<{ [id: string]: User }>("minsk::auth::loggined", {})
 
 export const useLogginedCache = () => {
   const [logginedCache, setlogginedCache] = useAtom(logginedCacheAtom)
 
   const addLoggined = (user: User) => {
-    const newUser = [user, ...logginedCache]
-    setlogginedCache(newUser)
+    setlogginedCache({
+      ...logginedCache,
+      [user.id]: user,
+    })
   }
 
   return {
