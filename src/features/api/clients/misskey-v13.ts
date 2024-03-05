2d450148e767592bcf0c0638bbcc1913446bbb74
山田ハヤオ <shun819.mail@gmail.com>
2024-03-05 17:40:51 +0900

    Change fetchEmojiUrl to getEmojiUrl

--- src/features/api/clients/misskey-v13.ts
+++ src/features/api/clients/misskey-v13.ts
@@ -4,7 +4,7 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV13Client extends MisskeyLatestClient {
   id = "misskey-v13"
 
-  override async fetchEmojiUrl(name: string): Promise<string | null> {
+  override async getEmojiUrl(name: string): Promise<string | null> {
     const { emojis } = await super.get("emojis")
     return emojis?.find?.((e: Record<keyof Emoji, unknown>) => e.name == name)?.url ?? null
   }
