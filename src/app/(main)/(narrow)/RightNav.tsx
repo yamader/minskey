b45aa740c7d20c629285e65f003b8d070cc50968
YamaD <me@dyama.net>
2024-03-09 13:28:26 +0900

    fix layout

--- src/app/(main)/(narrow)/RightNav.tsx
+++ src/app/(main)/(narrow)/RightNav.tsx
@@ -6,12 +6,12 @@ export default function RightNav() {
     <div className={w}>
       <div className={clsx(w, "fixed flex h-full flex-col gap-4 border-l pl-8 pt-2.5")}>
         <input className="rounded-full bg-gray-200 px-4 py-2" placeholder="検索" />
-        <p className="px-4 font-inter text-xs text-gray-600">
+        <div className="px-4 font-inter text-xs text-gray-600">
           <a className="hover:underline" href="/about/">
             minskeyについて
           </a>
-          <span className="mt-1">&copy; 2024 YamaD</span>
-        </p>
+          <p className="mt-1">&copy; 2024 YamaD</p>
+        </div>
       </div>
     </div>
   )
