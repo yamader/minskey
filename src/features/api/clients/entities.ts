9b7b2e2113b56be0b477deb5a76cd1b75b51821b
hayao <hayao@fascode.net>
2024-03-09 20:37:07 +0900

    Add note type

--- src/features/api/clients/entities.ts
+++ src/features/api/clients/entities.ts
@@ -1,4 +1,4 @@
-export type UserLite = {
+export interface UserLite {
   id: string
   name: string
   username: string
@@ -12,7 +12,7 @@ export type UserLite = {
   }[]
 }
 
-export type UserDetail = UserLite & {
+export interface UserDetail extends UserLite {
   isAdmin: boolean
   isModerator: boolean
   isBot: boolean
@@ -20,3 +20,39 @@ export type UserDetail = UserLite & {
 }
 
 export type User = UserLite | UserDetail
+
+export interface Note {
+  id: string
+  createdAt: string
+  deletedAt: string | null
+  text: string | null
+  cw: string | null
+  userId: string
+  user: UserLite
+  replyId: string | null
+  renoteId: string | null
+  reply: Note | null
+  renote: Note | null
+  isHidden: boolean
+  visibility: "public" | "home" | "followers" | "specified"
+  mentions: string[]
+  visibleUserIds: string[]
+  fileIds: string[]
+  files: unknown[] // TODO
+  tags: string[]
+  poll: unknown | null // TODO
+  emojis: unknown[] // TODO
+  channelId: string | null
+  channel: unknown | null // TODO
+  localOnly: boolean
+  reactionAcceptable: string | null
+  reactionEmojis: unknown // TODO
+  reactions: unknown // TODO
+  renoteCount: number
+  repliesCount: number
+  uri: string
+  url: string
+  reactionAndUserPairCache: string[]
+  clippedCount: number
+  myReaction: string | null
+}
--- src/features/api/clients/misskey-v12.ts
+++ src/features/api/clients/misskey-v12.ts
@@ -4,7 +4,8 @@ import MisskeyLatestClient from "./misskey-latest"
 export default class MisskeyV12Client extends MisskeyLatestClient {
   id = "misskey-v12"
 
-  override async getEmojiUrl(name: string): Promise<string | null> {
+  override async emojiUrl(name: string): Promise<string | null> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const { emojis } = await super.post<any>("meta")
     return emojis?.find?.((e: Record<keyof Emoji, unknown>) => e.name == name)?.url ?? null
   }
