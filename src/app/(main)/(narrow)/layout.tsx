3b124b355fd9627c2abce4839f06c06d0197cfe1
hayao <hayao@fascode.net>
2024-03-10 10:11:07 +0900

    Revert 885081dad3e60017d986c9bef7489e1992d64bad

--- src/app/(main)/(narrow)/layout.tsx
+++ src/app/(main)/(narrow)/layout.tsx
@@ -1,15 +1,6 @@
-"use client"
-
-import { useMfmProvider } from "~/features/common/mfm"
-import { useNotificationsStream } from "~/features/notifications"
-import { useTLStream } from "~/features/timeline"
 import RightNav from "./RightNav"
 
 export default function MainNarrowLayout({ children }: { children: React.ReactNode }) {
-  useMfmProvider()
-  useTLStream()
-  useNotificationsStream()
-
   return (
     <>
       <div className="flex w-[36rem] flex-col">{children}</div>
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { useMfmProvider } from "~/features/common/mfm"
 import NoteDialog from "~/features/note/NoteDialog"
+import { useNotificationsStream } from "~/features/notifications"
+import { useTLStream } from "~/features/timeline"
 import LeftNav from "./LeftNav"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  useMfmProvider()
+  useTLStream()
+  useNotificationsStream()
+
   return (
     <>
       <div className="flex h-screen justify-center">
