25228f6983101983ce4899169ab062ace77b833c
hayao <hayao@fascode.net>
2024-03-15 00:08:24 +0900

    Add link

--- src/app/(main)/BottomNav.tsx
+++ src/app/(main)/BottomNav.tsx
@@ -1,13 +1,20 @@
 import { Bell, Home, Settings } from "lucide-react"
+import Link from "next/link"
 
 const iconSize: string | number | undefined = 24
 
 export default function BottomNav() {
   return (
     <div className="fixed bottom-0 flex w-[36rem] justify-evenly bg-white p-3 lg:hidden">
-      <Home size={iconSize} />
-      <Bell size={iconSize} />
-      <Settings size={iconSize} />
+      <Link href="/home/">
+        <Home size={iconSize} />
+      </Link>
+      <Link href="/notifications/">
+        <Bell size={iconSize} />
+      </Link>
+      <Link href="/settings/">
+        <Settings size={iconSize} />
+      </Link>
     </div>
   )
 }
