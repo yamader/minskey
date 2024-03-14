e3f0cd20f5089a4051d3f255dd86d057b12b43c8
hayao <hayao@fascode.net>
2024-03-14 23:54:04 +0900

    Add buttons

--- src/app/(main)/BottomNav.tsx
+++ src/app/(main)/BottomNav.tsx
@@ -1,3 +1,13 @@
+import { Bell, Home, Settings } from "lucide-react"
+
+const iconSize: string | number | undefined = 24
+
 export default function BottomNav() {
-  return <div className="lg:hidden">hoge</div>
+  return (
+    <div className="fixed bottom-0 flex w-[36rem] justify-evenly bg-white p-3 lg:hidden">
+      <Home size={iconSize} />
+      <Bell size={iconSize} />
+      <Settings size={iconSize} />
+    </div>
+  )
 }
