cf6570052975f1bad8ccc0a817c28e68de2fcb2f
Yamada Hayao <hayao@fascode.net>
2024-03-15 21:38:30 +0900

    Support video (#75)

--- src/features/drive/FilePreview.tsx
+++ src/features/drive/FilePreview.tsx
@@ -1,13 +1,16 @@
+import { Link } from "lucide-react"
 import { entities } from "misskey-js"
 import Image from "next/image"
 
 export default function FilePreview({ file }: { file: entities.DriveFile }) {
   if (file.type.startsWith("image/")) {
     return <Image className="w-fit" src={file.url} width={125} height={125} alt="File" />
+  } else if (file.type.startsWith("video/")) {
+    return <video className="w-full" src={file.url} controls />
   } else {
     return (
       <>
-        {file.url}({file.type})
+        <Link href={file.url}>{file.name}</Link> ({file.type})
       </>
     )
   }
